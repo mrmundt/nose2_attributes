34eccebd4bd3b7950af91811ff2c637bf87edc7a
Miranda Mundt <55767766+mrmundt@users.noreply.github.com>
2020-03-23 15:50:14 -0600

    Make human readable.

--- src/main/main.py
+++ src/main/main.py
@@ -1,1 +1,30 @@
-"""Main:    A simple program that allows testing of nose2"""def printWords(words):    """    Takes a set of words and prints them individually.    Parameters    ----------    words : List of strings to be printed.    """    for word in words:        print(word)            return words        def main():    """    Main function.    """    words = ("Hello", "World!")    printWords(words)    if __name__ == "__main__":    main()
+"""
+Main:
+    A simple program that allows testing of nose2
+"""
+
+def printWords(words):
+    """
+    Takes a set of words and prints them individually.
+
+    Parameters
+    ----------
+    words : List of strings to be printed.
+
+    """
+    for word in words:
+        print(word)
+        
+    return words
+        
+def main():
+    """
+    Main function.
+
+    """
+    words = ("Hello", "World!")
+    printWords(words)
+    
+
+if __name__ == "__main__":
+    main()
