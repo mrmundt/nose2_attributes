f4cd7139a75c2c0b9de836bc4d86c38c7efd388b
Miranda Mundt <55767766+mrmundt@users.noreply.github.com>
2020-03-23 15:49:29 -0600

    Make human readable.

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,1 +1,13 @@
-"""Test_main:    Tests the printWords """import unittestfrom src.tests.utils import categoryfrom src.main.main import printWordsclass MyTestCase(unittest.TestCase):    @category('normal', 'fun')    def test_printWords(self):        self.assertEqual(printWords(['hello']), ['hello'])
+"""
+Test_main:
+    Tests the printWords function
+"""
+
+import unittest
+from src.tests.utils import category
+from src.main.main import printWords
+
+class MyTestCase(unittest.TestCase):
+    @category('normal', 'fun')
+    def test_printWords(self):
+        self.assertEqual(printWords(['hello']), ['hello'])
